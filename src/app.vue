7bb9134eccb6fb35dfc5ed82d1a02abf33f6e843
Truong Duy <quocduy13579113@gmail.com>
2025-06-22 19:13:02 +0700

    preload

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-if (import.meta.client) {
-    const preload = document.createElement('link')
-    preload.rel = 'preload'
-    preload.as = 'image'
-    preload.href = 'https://image.mavangthangloi.com/_w300/storage/logos/mavangthangloi.png'
-    document.head.appendChild(preload)
-}
+useHead({
+    link: [
+        {
+            rel: 'preload',
+            as: 'image',
+            href: 'https://image.mavangthangloi.com/_w300/storage/logos/mavangthangloi.png',
+        },
+    ],
+})
 </script>
 
 <template>
